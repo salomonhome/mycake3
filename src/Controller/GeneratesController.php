d27e16ff2d708261d9166914a577676f50bc75fc
salomon <salomonthomas123@gmail.com>
2018-06-26 15:51:07 +0530

    curd finish

---
+++ src/Controller/GeneratesController.php
@@ -0,0 +1,15 @@
+<?php
+   namespace App\Controller;
+   use App\Controller\AppController;
+   use Cake\ORM\TableRegistry;
+   use Cake\Datasource\ConnectionManager;
+
+   class GeneratesController extends AppController{
+      public function index(){
+      }
+      public function maxmil()
+      {
+        //echo "redirecting from action2";
+      }
+   }
+?>
